01afc5407bc4dcaf8ee8996aca5bdd2f903967e8
daniel-grinevich <kgbdannn@gmail.com>
2023-09-26 21:19:45 -0400

    idk

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -15,7 +15,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # take environment variables from .env.
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
